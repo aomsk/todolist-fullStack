94ce1faeb8b64d81bc8cb4303e73aef5a380e288
Sipang K <plaaom39@gmail.com>
2023-08-11 23:17:54 +0700

    feat: add search by status route

--- backend/src/controllers/todos.ts
+++ backend/src/controllers/todos.ts
@@ -9,3 +9,28 @@ export const getAllTodos = async (req: Request, res: Response) => {
     return res.sendStatus(400);
   }
 };
+
+export const searchTodosByStatus = async (req: Request, res: Response) => {
+  try {
+    const { status } = req.params;
+    const todos = await getTodos();
+
+    if (status === "complete") {
+      const todoComplete = todos.filter((todo) => todo.complete === true);
+      return res
+        .status(200)
+        .json({ message: "GET todo by status complete", status: "complete", todoComplete });
+    } else if (status === "notcomplete") {
+      const todoNotComplete = todos.filter((todo) => todo.complete === false);
+      return res.status(200).json({
+        message: "GET todo by status not-complete",
+        status: "notcomplete",
+        todoNotComplete,
+      });
+    } else {
+      return res.status(404).json({ message: "Not found status" });
+    }
+  } catch (error) {
+    return res.sendStatus(400);
+  }
+};
--- backend/src/router/todos.ts
+++ backend/src/router/todos.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { getAllTodos } from "../controllers/todos";
+import { getAllTodos, searchTodosByStatus } from "../controllers/todos";
 
 // Create Router
 export default (router: Router) => {
   router.get("/api/todos", getAllTodos);
+  router.get("/api/todos/search/:status", searchTodosByStatus);
 };
