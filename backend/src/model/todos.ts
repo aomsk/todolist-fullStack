28b77b6f425438f66b1fbfa7656156b3272e9afb
Sipang K <plaaom39@gmail.com>
2023-08-14 11:52:18 +0700

    feat: add new actions (createTodo, deleteTodo, updateTodo)

--- backend/src/model/todos.ts
+++ backend/src/model/todos.ts
@@ -11,3 +11,13 @@ export const TodoModel = mongoose.model("Todos", TodoSchema);
 
 // Actions
 export const getTodos = () => TodoModel.find();
+export const createTodo = (values: Record<string, any>) => {
+  const newTodo = new TodoModel(values)
+    .save()
+    .then((todo) => todo.toObject())
+    .catch((error) => console.log(error));
+  return newTodo;
+};
+export const deleteTodo = (id: string) => TodoModel.findByIdAndDelete({ _id: id });
+export const updateTodo = (id: string, values: Record<string, any>) =>
+  TodoModel.findByIdAndUpdate(id, values);
